9d54219e959e01bd201ce3dd27884624af68d8f6
chchedia <chebichedia@gmail.com>
2018-01-16 17:46:35 +0100

    Create routes, controllers and views of two actions: hello and byebye

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,24 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+
+    public function indexAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',array('nom' => 'Chedia'));
+        return new Response($content);
+    }
+
+    public function byebyeAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:byebye.html.twig',array('nom' => 'Chedia'));
+        return new Response($content);
+    }
+}
