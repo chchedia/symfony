15f4c6fea4c907b0aa20d37688ef7e8d57420530
chchedia <chebichedia@gmail.com>
2018-01-19 16:25:05 +0100

    add contactAction and the falsh message

--- src/OC/CoreBundle/Controller/CoreController.php
+++ src/OC/CoreBundle/Controller/CoreController.php
@@ -10,11 +10,19 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CoreController extends Controller
 {
-
+    //la page d'accueil
     public function indexAction()
     {
         return $this->render("OCCoreBundle:Core:index.html.twig");
     }
 
+    //la page contact
+    public function contactAction(Request $request){
+        $session= $request->getSession();
+
+        $session->getFlashBag()->add('info','La page Contact n\'est pas encore disponible');
+
+        return $this->redirectToRoute('oc_core_homepage');
+    }
 
 }
